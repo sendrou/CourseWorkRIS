efecf630e8ed7f5c08e88f7896c8bd011e28f669
sendrou <55048208+sendrou@users.noreply.github.com>
2024-12-17 19:04:41 +0300

    Add config and start.bat

---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+mpiexec -n 25 "D:\riscourse\RIS\StartMPI\bin\Debug\StartMPI"
+echo "Скрипт завершен, нажмите любую клавишу для выхода."
+pause
---
+++ startclient.bat
@@ -0,0 +1,2 @@
+@echo off
+start "Client" "D:\riscourse\RIS\Client\bin\Debug\net8.0-windows\Client.exe"
